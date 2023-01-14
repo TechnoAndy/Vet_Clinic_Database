7c508219c470a9d7e996066a6533df4adefa424f
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-01-14 15:17:28 +0200

    Add species column to table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,15 @@
+/* Database schema to keep the structure of entire database. */
 
+create database vet_clinic;
+
+
+CREATE TABLE animals (
+    id serial PRIMARY KEY NOT NULL,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
+);
+
+ALTER TABLE animals ADD COLUMN species varchar(250);

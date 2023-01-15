c06ef58584287f85311e1322a1f8704ba7bbf0c0
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-01-15 20:47:48 +0200

    Create new tables for joining

--- schema.sql
+++ schema.sql
@@ -1,1 +1,58 @@
+/* Database schema to keep the structure of entire database. */
 
+create database vet_clinic;
+
+
+CREATE TABLE animals (
+    id serial PRIMARY KEY NOT NULL,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
+);
+
+ALTER TABLE animals ADD COLUMN species varchar(250);
+
+/* Create owners table */
+CREATE TABLE owners (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, full_name varchar(250), age int);
+
+/* Create species table */
+CREATE TABLE species (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, name varchar(250));
+
+/* Modify animals table (ID already set to auto_increment while creating the table on line no.5) */
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id int;
+
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id int;
+
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+-- Join table
+
+CREATE TABLE vets(
+    id BIGSERIAL NOT NULL,
+    name VARCHAR(100),
+    age INT,
+    date_of_graduation DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE specializations(
+    vet_id INT,
+    species_id INT,
+    PRIMARY KEY(vet_id, species_id),
+    FOREIGN KEY(vet_id) REFERENCES vets(id),
+    FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+    vet_id INT,
+    animal_id INT,
+    date DATE,
+    PRIMARY KEY(vet_id, animal_id, date),
+    FOREIGN KEY(vet_id) REFERENCES vets(id),
+    FOREIGN KEY(animal_id) REFERENCES animals(id)
+);

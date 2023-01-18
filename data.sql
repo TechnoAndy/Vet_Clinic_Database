6450a0ee98d49c7486046dd23a412e08d185c6f1
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-01-18 10:35:50 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,37 +1,22 @@
 /* Populate database with sample data. */
 
+/* Insert animals data */
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Agumon', '02-03-2020', 0, true, 10.23);
+       ('Gabumon', '11-15-2018', 2, true, 8);
+       ('Pikachu', '01-07-2021', 1, false, 15.04);
+       ('Devimon', '05-12-2017', 5, true, 11);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Gabumon', '11-15-2018', 2, true, 8);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Pikachu', '01-07-2021', 1, false, 15.04);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Devimon', '05-12-2017', 5, true, 11);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Charmander', '02-08-2020', 0, false, -11), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Plantmon', '11-15-2021', 2, true, -5.7), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Squirtle', '04-02-1993', 3, false, -12.13), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Angemon', '06-12-2005', 1, true, -45), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Boarmon', '06-07-2005', 7, true, 20.4), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Boarmon', '06-07-2005', 7, true, 20.4), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Ditto', '05-14-2022', 4, true, 22);
+       ('Plantmon', '11-15-2021', 2, true, -5.7), 
+       ('Squirtle', '04-02-1993', 3, false, -12.13), 
+       ('Angemon', '06-12-2005', 1, true, -45), 
+       ('Boarmon', '06-07-2005', 7, true, 20.4), 
+       ('Blossom', '13-10-1198', 3, true, 17), 
+       ('Ditto', '05-14-2022', 4, true, 22);
+       
 -- Owners table
 
 INSERT INTO owners (full_name, age)
@@ -91,40 +76,7 @@ UPDATE animals
   SET owner_id = (SELECT id from owners WHERE full_name = 'Dean Winchester')
   WHERE name IN ('Angemon', 'Boarmon');
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Agumon', '02-03-2020', 0, true, 10.23);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Gabumon', '11-15-2018', 2, true, 8);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Pikachu', '01-07-2021', 1, false, 15.04);
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Devimon', '05-12-2017', 5, true, 11);
-
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Charmander', '02-08-2020', 0, false, -11), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Plantmon', '11-15-2021', 2, true, -5.7), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Squirtle', '04-02-1993', 3, false, -12.13), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Angemon', '06-12-2005', 1, true, -45), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Boarmon', '06-07-2005', 7, true, 20.4), 
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Boarmon', '06-07-2005', 7, true, 20.4), 
-
-
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
-VALUES ('Ditto', '05-14-2022', 4, true, 22);
 -- Owners table
 
 INSERT INTO owners (full_name, age)

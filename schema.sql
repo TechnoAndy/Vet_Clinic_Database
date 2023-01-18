529dde1aab77a590491b0334054e20f8f1550c07
Andrea Tamara Diegaardt <atdiegaardt@gmail.com>
2023-01-18 10:24:54 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
+/* Create database */
 create database vet_clinic;
 
-
+/* Create animals table */
 CREATE TABLE animals (
     id serial PRIMARY KEY NOT NULL,
     name varchar(100),
@@ -12,6 +13,7 @@ CREATE TABLE animals (
     weight_kg decimal
 );
 
+/* Alter animals table */
 ALTER TABLE animals ADD COLUMN species varchar(250);
 
 /* Create owners table */
@@ -20,18 +22,14 @@ CREATE TABLE owners (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, f
 /* Create species table */
 CREATE TABLE species (id int GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL, name varchar(250));
 
-/* Modify animals table (ID already set to auto_increment while creating the table on line no.5) */
+/* Alter animals table */
 ALTER TABLE animals DROP COLUMN species;
-
 ALTER TABLE animals ADD COLUMN species_id int;
-
 ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
-
 ALTER TABLE animals ADD owner_id int;
-
 ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
--- Join table
 
+/* Create vets table */
 CREATE TABLE vets(
     id BIGSERIAL NOT NULL,
     name VARCHAR(100),
@@ -40,6 +38,7 @@ CREATE TABLE vets(
     PRIMARY KEY(id)
 );
 
+/* Create specializations table */
 CREATE TABLE specializations(
     vet_id INT,
     species_id INT,
@@ -48,6 +47,7 @@ CREATE TABLE specializations(
     FOREIGN KEY(species_id) REFERENCES species(id)
 );
 
+/* Create visits table */
 CREATE TABLE visits(
     vet_id INT,
     animal_id INT,
